44336b43c5190c79f532f654b6f0cc6fe64b2893
valeriacampdl <valeriacamp1@hotmail.com>
2024-08-12 15:12:56 -0600

    Barra de vida
    barra de vida agregada, falta animación y código del combate del jugador

---
+++ Malamen/Assets/MenuPausa.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+
+    [SerializeField] private GameObject botonPausa;
+
+    [SerializeField] private GameObject menuPausa;
+
+    private bool juegoPausado = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+
+    public void Pausa()
+    {
+        juegoPausado = true;
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Cerrando Juego");
+        Application.Quit();
+    }
+
+}
