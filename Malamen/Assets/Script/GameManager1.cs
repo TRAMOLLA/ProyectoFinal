f90061ba36a744717525f4238768fca57a855525
valeriacampdl <valeriacamp1@hotmail.com>
2024-08-15 13:06:03 -0600

    plataforma
    plataforma final

--- Malamen/Assets/Script/GameManager1.cs
+++ Malamen/Assets/Script/GameManager1.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager1 : MonoBehaviour
 {
+    public GameManager1 instance;
+
+    aw
     // Start is called before the first frame update
     void Start()
     {
