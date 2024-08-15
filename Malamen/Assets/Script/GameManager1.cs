a9ba92e17cd803afbe75b8da560838516fefd7d9
valeriacampdl <valeriacamp1@hotmail.com>
2024-08-15 13:24:13 -0600

    Revert "plataforma"
    This reverts commit f90061ba36a744717525f4238768fca57a855525.

--- Malamen/Assets/Script/GameManager1.cs
+++ Malamen/Assets/Script/GameManager1.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class GameManager1 : MonoBehaviour
 {
-    public GameManager1 instance;
-
-    aw
     // Start is called before the first frame update
     void Start()
     {
