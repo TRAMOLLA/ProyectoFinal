47af339bae842b8693ec7128a7a86e7b031a6209
valeriacampdl <valeriacamp1@hotmail.com>
2024-08-16 15:44:14 -0600

    cinemática lista
    no cambia de páginas y cambio de escena

---
+++ Malamen/Assets/CambioIntro.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioIntro : MonoBehaviour
+{
+    public GameObject[] pantallas;
+    public int pantalla;
+
+    public void Cambio()
+    {
+        pantalla += 1;
+        for (int i = 0; i < pantallas.Length; i++)
+        {
+            if ( i == pantalla)
+            {
+                pantallas[i].SetActive(true);
+            }
+            else
+            {
+                pantallas[i].SetActive(false);
+            }
+        }
+    }
+}
